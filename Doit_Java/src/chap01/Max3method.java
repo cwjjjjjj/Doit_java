7f1128583c664e6859aaa71186f8a4d132b069db
woojincheon <qpqpqp0102@naver.com>
2023-08-02 21:20:51 +0900

    second

---
+++ Doit_Java/src/chap01/Max3method.java
@@ -0,0 +1,14 @@
+package chap01;
+
+class Max3method {
+	static int max3(int a, int b, int c) {
+		int max = a;
+		if (b > max) {
+			max = b;
+		}
+		if (c > max) {
+			max = c;
+		}
+		return max;
+	}
+}
---
+++ Doit_Java/src/chap01/input.java
@@ -0,0 +1,10 @@
+package chap01;
+
+import java.util.Scanner;
+
+class input {
+public static void main(String[] args) {
+	Scanner stdIn = new Scanner(System.in);
+	stdIn.nextInt();
+}
+}
