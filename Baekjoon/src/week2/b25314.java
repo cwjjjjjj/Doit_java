ce591b3140c102a8a22cfb6f3c6224d530738ef8
woojincheon <qpqpqp0102@naver.com>
2023-08-08 23:38:06 +0900

    0808 2

--- Baekjoon/src/week2/b25314.java
+++ Baekjoon/src/week2/b25314.java
@@ -2,14 +2,15 @@ package week2;
 
 import java.util.Scanner;
 
-public class b2739 {
-	
+public class b25314 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
 		
-		for (int i=1; i<=9; i++) {
-			System.out.println(N +" * " + i + " = "+ N*i);
-		}
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int i = N/4;
+		System.out.println("long ".repeat(i) + "int");
+		
 	}
+
 }
